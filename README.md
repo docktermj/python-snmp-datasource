44e64bc5c3bc074758e4776e64b8c54047c8b360
Michael Dockter <michael@dockter.com>
2017-09-14 17:16:07 -0400

    Working example.  ...still needs tweaking

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # python-snmp-datasource
+
+## Usage
+
+A wrapper over `snmpget` that supports configuration via inputfile.
+
+Input via `stdin` and output via `stdout` are symmetrical.
+
+### Invocation
+
+```console
+cat test.txt | snmp_datasource.py
+```
+
+Where [test.txt](./test.txt) is an example file.
+
+
+By using [go-stdin-sleep-stdout](https://github.com/docktermj/go-stdin-sleep-stdout),
+a more sophisticated test is this:
+
+```console
+cat test.txt | ./snmp_datasource.py | go-stdin-sleep-stdout --sleep 1 | ./snmp_datasource.py
+```
+
+The SNMP metrics returned in `stdout` can be piped to the `stdin` of a subsequent invocation of `snmp_datasource.py`.
+
+## Development
+
+### Dependencies
+
+#### Set environment variables
+
+```console
+export PROJECT_DIR="${HOME}/src/github.com/docktermj"
+export REPOSITORY_DIR="${PROJECT_DIR}/python-snmp-datasource"
+```
+
+#### Download project
+
+```console
+mkdir -p ${PROJECT_DIR}
+cd ${PROJECT_DIR}
+git clone git@github.com:docktermj/python-snmp-datasource.git
+```
--- snmp_datasource.py
+++ snmp_datasource.py
@@ -1,140 +1,126 @@
 #!/usr/bin/python
 # encoding: utf-8
 '''
-test -- shortdesc
-
-test is a description
-
-It defines classes_and_methods
-
-@author:     user_name
-
-@copyright:  2017 organization_name. All rights reserved.
-
-@license:    license
-
-@contact:    user_email
-@deffield    updated: Updated
+snmp_datasource -- A wrapper for `snmpget` that uses `stdin` for configuration.
 '''
 
-from argparse import ArgumentParser
-from argparse import RawDescriptionHelpFormatter
-import os
+import fileinput
+import logging
+import subprocess
 import sys
+import time
 
 
 __all__ = []
-__version__ = 0.1
+__version__ = 1.0
 __date__ = '2017-09-14'
 __updated__ = '2017-09-14'
 
-DEBUG = 1
-TESTRUN = 0
-PROFILE = 0
+# "snmpget" result array indicies.
+
+SNMP_NAME = 0
+SNMP_EQUALS = 1
+SNMP_DATATYPE = 2
+SNMP_VALUE = 3
+
+# MIB names.
+
+MIB_MY_SEPARATOR = "___"
+MIB_MY = "my::"
+MIB_MY_RATE = "my" + MIB_MY_SEPARATOR
+MIB_MY_IP_ADDRESS = "my::ip-address"
+MIB_MY_TIMESTAMP = "my::timestamp"
+
+#  Define input and output  dictionaries
+
+inputData = {}
+inputMetaData = {}
+inputRate = {}
+snmpData = {}
+outputData = {}
 
 
-class CLIError(Exception):
-    '''Generic exception to raise and log different fatal errors.'''
-    def __init__(self, msg):
-        super(CLIError).__init__(type(self))
-        self.msg = "E: %s" % msg
+def process_line(line):
+    ''' Process a line in the format: "MetricName = Datatype: value" '''
 
-    def __str__(self):
-        return self.msg
-
-    def __unicode__(self):
-        return self.msg
-
-
-def main(argv=None):  # IGNORE:C0111
-    '''Command line options.'''
-
-    if argv is None:
-        argv = sys.argv
+    splits = line.split()
+    if splits[SNMP_NAME].startswith(MIB_MY):
+        inputMetaData[splits[SNMP_NAME]] = splits
+    elif splits[SNMP_NAME].startswith(MIB_MY_RATE):
+        inputRate[splits[SNMP_NAME]] = splits
     else:
-        sys.argv.extend(argv)
+        inputData[splits[SNMP_NAME]] = splits
 
-    program_name = os.path.basename(sys.argv[0])
-    program_version = "v%s" % __version__
-    program_build_date = str(__updated__)
-    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
-    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
-    program_license = '''%s
 
-  Created by user_name on %s.
-  Copyright 2017 organization_name. All rights reserved.
+def read_stdin():
+    ''' Read the configuration input from "stdin". '''
 
-  Licensed under the Apache License 2.0
-  http://www.apache.org/licenses/LICENSE-2.0
+    for line in fileinput.input():
+        process_line(line)
+    if MIB_MY_TIMESTAMP not in inputMetaData:
+        inputMetaData[MIB_MY_TIMESTAMP] = [MIB_MY_TIMESTAMP, '=', "Counter32", "0"]
 
-  Distributed on an "AS IS" basis without warranties
-  or conditions of any kind, either express or implied.
 
-USAGE
-''' % (program_shortdesc, str(__date__))
+def read_snmp():
+    ''' Make call to snmpget to get current values. '''
 
-    try:
-        # Setup argument parser
-        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-r", "--recursive", dest="recurse", action="store_true", help="recurse into subfolders [default: %(default)s]")
-        parser.add_argument("-v", "--verbose", dest="verbose", action="count", help="set verbosity level [default: %(default)s]")
-        parser.add_argument("-i", "--include", dest="include", help="only include paths matching this regex pattern. Note: exclude is given preference over include. [default: %(default)s]", metavar="RE" )
-        parser.add_argument("-e", "--exclude", dest="exclude", help="exclude paths matching this regex pattern. [default: %(default)s]", metavar="RE" )
-        parser.add_argument('-V', '--version', action='version', version=program_version_message)
-        parser.add_argument(dest="paths", help="paths to folder(s) with source file(s) [default: %(default)s]", metavar="path", nargs='+')
+    ip_address = inputMetaData[MIB_MY_IP_ADDRESS][SNMP_VALUE]
+    args = ['snmpget', '-v', '1', '-c', 'public', ip_address] + inputData.keys()
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if err != "":
+        logging.error("ERROR: p.communicate() failed: " + err)
+        return
 
-        # Process arguments
-        args = parser.parse_args()
+    # Parse snmpget output stream into a dictionary of arrays.
 
-        paths = args.paths
-        verbose = args.verbose
-        recurse = args.recurse
-        inpat = args.include
-        expat = args.exclude
+    for line in out.splitlines():
+        splits = line.split()
+        snmpData[splits[SNMP_NAME]] = splits
 
-        if verbose > 0:
-            print("Verbose mode on")
-            if recurse:
-                print("Recursive mode on")
-            else:
-                print("Recursive mode off")
 
-        if inpat and expat and inpat == expat:
-            raise CLIError("include and exclude pattern are equal! Nothing will be processed.")
+def calculate_output_data():
+    ''' Fill in the outputData dictionary. '''
 
-        for inpath in paths:
-            ### do something with inpath ###
-            print(inpath)
-        return 0
-    except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
-        return 0
-    except Exception, e:
-        if DEBUG or TESTRUN:
-            raise(e)
-        indent = len(program_name) * " "
-        sys.stderr.write(program_name + ": " + repr(e) + "\n")
-        sys.stderr.write(indent + "  for help use --help")
-        return 2
+    # Copy and update metadata.
 
+    for key, value in inputMetaData.items():
+        outputData[key] = value
+    outputData[MIB_MY_TIMESTAMP] = [MIB_MY_TIMESTAMP, '=', "Counter32", time.time()]
+
+    # Copy new SNMP data.
+
+    for key, value in snmpData.items():
+        outputData[key] = value
+
+    # Calculate any "per second" data.
+
+    deltaTime = outputData[MIB_MY_TIMESTAMP][SNMP_VALUE] - float(inputMetaData[MIB_MY_TIMESTAMP][SNMP_VALUE])
+    for key, value in inputRate.items():
+        parsedKey = key.split(MIB_MY_SEPARATOR)
+        originalKey = parsedKey[1]
+        deltaValue = int(snmpData[originalKey][SNMP_VALUE]) - int(inputData[originalKey][SNMP_VALUE])
+        outputData[key] = value  # Copy entire original array.
+        outputData[key][SNMP_VALUE] = deltaValue / deltaTime  # Update only the value.
+
+
+def write_output():
+    ''' Write output in MIB key sorted order. '''
+
+    sortedKeys = sorted(outputData.keys())
+    for key in sortedKeys:
+        print("{} {} {} {}".format(*outputData[key]))
+
+
+def main(argv=None):
+    pass
+
+
+# Main
 
 if __name__ == "__main__":
-    if DEBUG:
-        sys.argv.append("-h")
-        sys.argv.append("-v")
-        sys.argv.append("-r")
-    if TESTRUN:
-        import doctest
-        doctest.testmod()
-    if PROFILE:
-        import cProfile
-        import pstats
-        profile_filename = 'test_profile.txt'
-        cProfile.run('main()', profile_filename)
-        statsfile = open("profile_stats.txt", "wb")
-        p = pstats.Stats(profile_filename, stream=statsfile)
-        stats = p.strip_dirs().sort_stats('cumulative')
-        stats.print_stats()
-        statsfile.close()
-        sys.exit(0)
+    read_stdin()
+    read_snmp()
+    calculate_output_data()
+    write_output()
     sys.exit(main())
